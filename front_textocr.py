a250e485f7fc6b873111315d8af2e4de56f02287
Imanol Daran <imanoldaran@Imanols-MacBook-Pro-4.local>
2024-03-25 15:52:22 -0600

    nanonets test

--- front_textocr.py
+++ front_textocr.py
@@ -3,13 +3,17 @@ from PIL import Image
 import pytesseract
 import openai
 import os
+import requests
+import io
+import json
 
 # Streamlit page configuration
 st.set_page_config(page_title="Tickets con AI", layout="wide")
 
 print("API Key:", os.getenv("OPENAI_API_KEY"))
 openai.api_key = os.getenv("OPENAI_API_KEY")
- 
+url = 'https://app.nanonets.com/api/v2/OCR/Model/438e29ff-02b8-497e-b1c2-06d9861d2ea2/LabelUrls/?async=false'
+
 
 
 def extract_text(image):
@@ -45,7 +49,7 @@ def parse_receipt_with_gpt(text):
 st.title("Tickets con IA")
 st.write("Sube o toma una foto de tu ticket.")
 
-tab1,tab2 = st.tabs(["Subir Foto", "Tomar Foto"])
+tab1,tab2, tab3 = st.tabs(["Subir Foto", "Tomar Foto", "Prueba con NanoNets"])
 
 with tab1:
     # File uploader allows user to add their own image
@@ -71,6 +75,27 @@ with tab2:
             parsed_text = parse_receipt_with_gpt(extracted_text)
             st.write("Texto obtenido:")
             st.json(parsed_text)
+
+with tab3:
+    # File uploader allows user to add their own image
+    uploaded_file = st.file_uploader("Selecciona tu archivo2", type=["jpg", "jpeg", "png"])
+    if uploaded_file is not None:
+       image = Image.open(uploaded_file)
+       st.image(image, caption="Ticket subido", use_column_width=True)
+       if st.button("Analizar con IA"):
+          with st.spinner('Extrayendo información y analizando...'):
+            with open(uploaded_file.name, "wb") as f:
+                f.write(uploaded_file.getvalue())
+            
+            data = {'file': open(uploaded_file.name, 'rb')}
+            response = requests.post(url, auth=requests.auth.HTTPBasicAuth('be416660-bfda-11ee-adf9-f63b86b2ddb7', ''), files=data)
+            print(response.text)
+            st.write("Texto obtenido:")
+            st.json(json.loads(response.text))
+            data['file'].close()
+            os.remove(uploaded_file.name)
+
+
 
 
 
